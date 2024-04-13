505bb2654efce1e20278560da2aa5b62b8ef69ce
aasim <aasim.ss@gmail.com>
2024-04-13 16:08:36 -0400

    Cloned feature/alerts-and-frames into feature/alerts-and-frames-with-plugin

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    supportFile: 'cypress/support/commands.js',
+    specPattern: 'cypress/integration/**/*.spec.js',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
