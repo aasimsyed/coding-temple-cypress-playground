d7d2d24b658f9f97ec022a1a32c6e3d6a1c611bd
aasim <aasim.ss@gmail.com>
2024-04-13 23:39:51 -0400

    optimized the alerts/frames specs

--- cypress/integration/alerts.spec.js
+++ cypress/integration/alerts.spec.js
@@ -1,36 +1,25 @@
 describe('Alerts Handling on DEMOQA', () => {
   beforeEach(() => {
     cy.visitWithAdBlocker('https://demoqa.com/alerts');
+    cy.clock();
   });
 
   it('tests the [Alert] button which shows an alert box', () => {
-    cy.on('window:alert', (text) => {
-      expect(text).to.contains('You clicked a button');
-    });
+    cy.on('window:alert', (text) => expect(text).to.contain('You clicked a button'));
     cy.get('#alertButton').click();
   });
 
   it('ensures the [Timer Alert] button triggers after exactly 5 seconds', () => {
-    cy.clock();
-
-    cy.on('window:alert', (text) => {
-      expect(text).to.equal('This alert appeared after 5 seconds');
-    });
-
+    cy.on('window:alert', (text) => expect(text).to.equal('This alert appeared after 5 seconds'));
     cy.get('#timerAlertButton').click();
-    
-    cy.get('#statusMessage').should('not.exist');
-
-    cy.tick(5000);
+    cy.tick(5000); // Simulate passing of 5 seconds
   });
-
 
   it('accepts a confirm alert and verifies actions taken on confirmation', () => {
     cy.on('window:confirm', (str) => {
       expect(str).to.eq('Do you confirm action?');
       return true;
     });
-
     cy.get('#confirmButton').click();
     cy.get('#confirmResult').should('contain', 'You selected Ok');
   });
@@ -40,16 +29,12 @@ describe('Alerts Handling on DEMOQA', () => {
       expect(str).to.eq('Do you confirm action?');
       return false;
     });
-
     cy.get('#confirmButton').click();
     cy.get('#confirmResult').should('contain', 'You selected Cancel');
   });
 
   it('accepts a prompt alert and verifies actions taken on confirmation', () => {
-    cy.window().then((win) => {
-      cy.stub(win, 'prompt').returns('Cypress Tester');
-    });
-
+    cy.window().then((win) => cy.stub(win, 'prompt').returns('Cypress Tester'));
     cy.get('#promtButton').click();
     cy.get('#promptResult').should('contain', 'You entered Cypress Tester');
   });
--- cypress/integration/frames.spec.js
+++ cypress/integration/frames.spec.js
@@ -3,29 +3,19 @@ describe('Frames Handling on DEMOQA', () => {
     cy.visitWithAdBlocker('https://demoqa.com/frames');
   });
 
-  // Test to check content and dimensions of frame1
-  it('verifies content and dimensions within frame1', () => {
-    cy.get('#frame1')
-      .should('have.attr', 'width', '500px')
-      .and('have.attr', 'height', '350px');
+  const verifyFrameContentAndSize = (frameId, expectedWidth, expectedHeight, expectedText) => {
+    it(`verifies content and dimensions within ${frameId}`, () => {
+      cy.get(frameId)
+        .should('have.attr', 'width', expectedWidth)
+        .and('have.attr', 'height', expectedHeight);
 
-    cy.frameLoaded('#frame1');
-    cy.iframe('#frame1').within(() => {
-      cy.get('h1#sampleHeading').should('exist')
-        .and('have.text', 'This is a sample page');
+      cy.frameLoaded(frameId);
+      cy.iframe(frameId).within(() => {
+        cy.get('h1#sampleHeading').should('have.text', expectedText);
+      });
     });
-  });
+  };
 
-  // Test to check content and dimensions of frame2
-  it('verifies content and dimensions within frame2', () => {
-    cy.get('#frame2')
-      .should('have.attr', 'width', '100px')
-      .and('have.attr', 'height', '100px');
-
-    cy.frameLoaded('#frame2');
-    cy.iframe('#frame2').within(() => {
-      cy.get('h1#sampleHeading').should('exist')
-        .and('have.text', 'This is a sample page');
-    });
-  });
+  verifyFrameContentAndSize('#frame1', '500px', '350px', 'This is a sample page');
+  verifyFrameContentAndSize('#frame2', '100px', '100px', 'This is a sample page');
 });
