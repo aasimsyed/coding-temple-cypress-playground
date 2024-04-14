38d4d21ce09f1475686c1c973f82f7061d980c04
aasim <aasim.ss@gmail.com>
2024-04-13 22:05:58 -0400

    refactored 'visitWithAdBlocker' to use fixture file

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,20 +2,74 @@ import 'cypress-iframe'
 
 // visitWithAdBlocker: Visits a page and blocks ads
 Cypress.Commands.add('visitWithAdBlocker', (url) => {
-  // Setup intercepts
-  cy.intercept('https://pagead2.googlesyndication.com/**', {statusCode: 200, body: ''}).as('blockAds');
-  cy.intercept('https://www.googletagservices.com/tag/js/gpt.js', {statusCode: 200, body: ''}).as('blockGpt');
-  cy.intercept('https://www.googletagmanager.com/gtm.js?id=GTM-MX8DD4S', {statusCode: 200, body: ''}).as('blockGtm');
-  cy.intercept('https://cdn.ad.plus/player/adplus.js', {statusCode: 200, body: ''}).as('blockAdPlus');
-  cy.intercept('**/*.gif', {statusCode: 200, body: ''}).as('blockGif');
-  cy.intercept('**/usermatch.gif**', {statusCode: 200, body: ''}).as('blockUserMatch');
-  cy.intercept('**/merge**', {statusCode: 200, body: ''}).as('blockMerge');
-  cy.intercept('**/*.js', (req) => {
-      if (req.url.includes('google') || req.url.includes('adsbygoogle')) {
-          req.destroy();
+  cy.fixture('adBlockerUrls').then((urls) => {
+    // Setup intercepts
+    Object.keys(urls).forEach((key) => {
+      if (key !== 'blockScripts') {
+        cy.intercept(urls[key], { statusCode: 200, body: '' }).as(key);
       }
-  }).as('blockScripts');
+    });
 
-  // Visit the page
-  cy.visit(url);
+    cy.intercept(urls['blockScripts'], (req) => {
+      if (req.url.includes('google') || req.url.includes('adsbygoogle')) {
+        req.destroy();
+      }
+    }).as('blockScripts');
+
+    // Visit the page
+    cy.visit(url);
+  });
+});
+
+// Custom command for logging into Sauce Demo
+Cypress.Commands.add('login', (username, password) => {
+  cy.get('[data-test="username"]').type(username);
+  cy.get('[data-test="password"]').type(password);
+  cy.get('[data-test="login-button"]').click();
+});
+
+// Custom command to login with a user type
+Cypress.Commands.add('loginWithFixture', (userType) => {
+  cy.fixture('credentials').then((credentials) => {
+    const user = credentials.users[userType];
+    cy.login(user.username, user.password);
+  });
+});
+
+// Custom command to add a single item to the cart by its name
+Cypress.Commands.add('addItemToCartByName', (itemName) => {
+  cy.contains('.inventory_item_name', itemName)
+    .parents('.inventory_item')
+    .within(() => {
+      cy.get('.btn_inventory').click();
+    });
+});
+
+// Custom command to add all items to the cart
+Cypress.Commands.add('addAllItemsToCart', () => {
+  cy.get('.inventory_item').each((element) => {
+    // For each .inventory_item, find the button within it and click
+    cy.wrap(element).find('.btn_inventory').click();
+  });
+});
+
+// Custom command to complete the checkout process
+Cypress.Commands.add('completeCheckout', (firstName, lastName, zipCode) => {
+  cy.get('.shopping_cart_link').click();
+  cy.get('.checkout_button').click();
+  cy.get('#first-name').type(firstName);
+  cy.get('#last-name').type(lastName);
+  cy.get('#postal-code').type(zipCode);
+  cy.get('.btn_primary.cart_button').click();
+  // This clicks the final 'Finish' button on the checkout page
+  cy.get('.btn_action.cart_button').click(); 
+});
+
+// Example of overwriting the 'visit' command to always disable the cache
+Cypress.Commands.overwrite('visit', (originalFn, url, options) => {
+  // Example usage of overwriting an existing command
+  // This will append cache busting query param to each visit
+  let noCacheStr = `_noCache=${Math.random()}`;
+  url.includes('?') ? (url += `&${noCacheStr}`) : (url += `?${noCacheStr}`);
+  return originalFn(url, options);
 });
