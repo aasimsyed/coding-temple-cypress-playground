61ded08f3d995ae1f2be98c057530e6b6226f36e
aasim <aasim.ss@gmail.com>
2024-04-16 00:57:18 -0400

    separated e2e and api tests into respective directories and updated config file to support

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,10 @@ module.exports = defineConfig({
   e2e: {
     chromeWebSecurity: false,
     supportFile: 'cypress/support/commands.js',
-    specPattern: 'cypress/integration/**/*.spec.js',
+    specPattern: [
+      'cypress/integration/e2e/**/*.spec.js',
+      'cypress/integration/api/**/*.spec.js'
+    ],
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
